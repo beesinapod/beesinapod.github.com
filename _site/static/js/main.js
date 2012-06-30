1f4ba64de5782004714020fe665e2225ed1f4b70
Matthew Owen <matthew.owen95@gmail.com>
2012-06-30 21:33:12 +0100

    better config

--- _site/static/js/main.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function() {
-    if(window.pageYOffset > $('header').height()) {
-        $('.ingredients').addClass('ing-fixed');
-    }
-
-    if(window.pageYOffset < $('header').height()) {
-        $('.ingredients').removeClass('ing-fixed');
-    }
-});
