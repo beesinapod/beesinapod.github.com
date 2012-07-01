e938e51a5ff346fd9413b51010a35de68e94a1af
Matthew Owen <matthew.owen95@gmail.com>
2012-07-01 22:09:51 +0100

    smoothly fit ingredient panel to bottom of content area

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
 $(window).scroll(function() {
 
-    if(window.pageYOffset > $('body').height() - $('.side').height() - $('footer').height()) {
+    if(window.pageYOffset > $('body').height() - $('.side').height() - $('footer').height() - 40) {
         $('.side').removeClass('side-fixed');
         $('.side').addClass('side-bottom');
     }
