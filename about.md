e77d0d1f02d98093fa6f9608239def39ff8cd8b4
Matthew Owen <matthew.owen95@gmail.com>
2012-07-01 19:13:15 +0100

    add about page, layout for pages

---
+++ about.md
@@ -0,0 +1,7 @@
+---
+layout: page
+---
+
+## About Bees in a Pod
+
+Bees in a Pod is where Matthew and Juliet write about cooking and eating food.
--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,16 @@
 $(window).scroll(function() {
 
-    if(window.pageYOffset > $('body').height() - $('.ingredients').height() - $('footer').height()) {
-        $('.ingredients').removeClass('ing-fixed');
-        $('.ingredients').addClass('ing-bottom');
+    if(window.pageYOffset > $('body').height() - $('.side').height() - $('footer').height()) {
+        $('.side').removeClass('side-fixed');
+        $('.side').addClass('side-bottom');
     }
 
     else if(window.pageYOffset > $('header').height()) {
-        $('.ingredients').addClass('ing-fixed');
-        $('.ingredients').removeClass('ing-bottom');
+        $('.side').addClass('side-fixed');
+        $('.side').removeClass('side-bottom');
     }
 
     else if(window.pageYOffset < $('header').height()) {
-        $('.ingredients').removeClass('ing-fixed');
+        $('.side').removeClass('side-fixed');
     }
 });
