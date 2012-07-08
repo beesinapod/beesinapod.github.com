4bd3ecf9915df6d7b007a68239832793e20a19cc
Matthew Owen <matthew.owen95@gmail.com>
2012-07-08 17:53:59 +0100

    updates to lightbox js

--- static/js/listToLight/ltl.js
+++ static/js/listToLight/ltl.js
@@ -15,6 +15,7 @@ function listToLight(selector) {
         },
 
         switchImage: function (img) {
+            var that = this;
             // change which image is shown in the lightbox
             $('.ltl-box .image')
                 .html($(img).clone());
@@ -43,6 +44,23 @@ function listToLight(selector) {
             } else {
                 $('.ltl-scroller.right').css('display', 'none');
             }
+            $(document).keydown(function (e) {
+                // We don't need to worry about unbinding these events
+                // This is because hideLightbox() doesn't do anything of
+                // significance if the lightbox is already hidden, and
+                // scrollGallery() only scrolls if there's something to
+                // scroll to.
+                console.log(e.keyCode);
+                if (e.keyCode === 27) {
+                    that.hideLightbox();
+                }
+                if (e.keyCode === 37) {
+                    that.scrollGallery('left');
+                }
+                if (e.keyCode === 39) {
+                    that.scrollGallery('right');
+                }
+            });
         },
 
         scrollGallery: function (dir) {
